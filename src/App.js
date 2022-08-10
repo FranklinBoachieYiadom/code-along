3e2dc8722c96863cd7075920bdec12a22a3ceaaa
FranklinBoachieYiadom <franklinboachie@gmail.com>
2022-08-10 15:37:31 +0000

    props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Header from "./components/Header";
-import {Button1, Button2, Button3 } from "./components/Button"
+import Button from "./components/Button";
 import {Image} from "./components/Images";
+import NavBar from "./components/NavBar";
 
 function App(){
-  return <div>
+  return (
+  <div>
+     <NavBar/>
     <Header/>
-    <Button1/>
-    <Button2/>
-    <Button3/>
-    <Image />
-    
-  </div>;
+<Button label= "Add to cart"/>
+<Button label= "Home"/> 
+<Button label= "About"/> 
+<Button label= "Register"/>     
+<Image />
+</div>
+); 
 }
 export default App;
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
-export const Button1=()=>{
-    return <button>Add to Cart</button>;
-};
-export const Button2=()=>{
-    return <button>Login</button>;
-};
-export const Button3=()=>{
-    return <button>Register</button>;
-};
+
+
+function Button({label}){
+    return <button className="border-2 border-blue-300 py-2 px-4 rounded-full text-blue-400 hover:bg-blue-400 hover:text-white">{label}
+    </button>;
+}
+export default Button;
+
+
+
+
 
