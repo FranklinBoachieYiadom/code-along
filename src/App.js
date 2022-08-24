f1e3f3fe48c3fdf89c41276d514491b14c921c01
FranklinBoachieYiadom <franklinboachie@gmail.com>
2022-08-24 14:20:32 +0000

    Routing-2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@
 import NavBar from "./components/NavBar";
 // import Practice from "./components/Practce";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ProductList from "./components/ProductList";
-import TaskManager from "./components/TaskManager";
+import ProductList from "./pages/ProductList";
+import TaskManager from "./pages/TaskManager";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-
+import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
+import Spinner from "./components/Spinner";
+import Product from "./components/Product";
 
 
 
@@ -22,8 +25,10 @@ function App(){
       <Route path= "/" element= {<Home/>}/>
       <Route path= "/task-manager" element= {<TaskManager/>}/>
       <Route path= "/product-list" element= {<ProductList/>}/>
+      <Route path= "/product-list/:productId" element= {<ProductDetail/>}/>
       <Route path= "/contact" element= {<Contact/>}/>
       <Route path= "/about" element= {<About/>}/>
+      <Route path="*" element={<NotFound/>}/>
      </Routes>
      </BrowserRouter>
 </div>
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,23 +1,12 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import Product from "./Product";
-import Spinner from "./Spinner";
+import Product from "../components/Product";
+import Spinner from "../components/Spinner";
 import useAxios from "../hooks/useAxios";
 
 function ProductList(){
         const {data, isLoading, error}=useAxios("https://api.escuelajs.co/api/v1/products");
-        // useEffect(()=>{
-        //     const getProducts= async()=>{
-        //         const results = await axios.get(
-        //             "https://api.escuelajs.co/api/v1/products"
-        //         );
-        //         setProducts(results.data);
-        //     };
-        //     getProducts();
-        // }, []);
-
-        // console.log(products);
-        
+    
         if(isLoading) return <Spinner/>;
 
         if (error)return <p>{error}</p>
