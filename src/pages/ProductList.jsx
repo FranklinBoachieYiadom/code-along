bf05fba83c5905b382c72901c6835c2c244d74d1
FranklinBoachieYiadom <franklinboachie@gmail.com>
2022-08-25 15:24:40 +0000

    state-management-1

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,21 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect,} from "react";
 import axios from "axios";
 import Product from "../components/Product";
-import Spinner from "../components/Spinner";
 import useAxios from "../hooks/useAxios";
+import Spinner from "../components/Spinner";
+import { useProductContext} from "../context/productContext";
 
 function ProductList(){
-        const {data, isLoading, error}=useAxios("https://api.escuelajs.co/api/v1/products");
-    
-        if(isLoading) return <Spinner/>;
+    // const [products, setProducts] = useState([])
+//    const {data, isLoading, error} = useAxios( "https://api.escuelajs.co/api/v1/products");
 
-        if (error)return <p>{error}</p>
-        return( 
+const {data, isLoading, error} = useAxios( 
+    "https://fakestoreapi.com/products");
+   
+const { products,setProducts } = useProductContext(); 
+       
+   
+   useEffect(() => {
+    setProducts(data);
+ }, [data] );
+
+    if (isLoading) return <Spinner />
+    if (error) return <p>{error}</p>
+
+     console.log(products);
+
+
+    return(
         <div className="flex flex-wrap gap-10 justify-center pt-10">
-            {data.map((data)=>(
-            <Product product={data}/>
-            ))}
-            </div>
-        );   
+            {products?.map((data) =>(
+                <Product product = {data} />
+                ))};
+        </div>   
+    );
 }
- export default ProductList;
+export default ProductList;
